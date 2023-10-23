0c37293b02c6f36c30366845ca358dd504f879a3
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-10-23 17:38:57 +0300

    js selectors

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const input = document.getElementById('input')
+const planet = document.getElementById('planet-select')
+const btn = document.getElementById('button')
+const img = document.getElementById('image')
+const message = document.querySelector('.result-message')
+const kg = document.querySelector('.result-kg')
+
